d54ceb8a7e6f2a825828ecea3445d7d6be148805
Brian Le <brianle1018@gmail.com>
2025-06-01 21:36:48 -0700

    Update auth.h

--- include/auth.h
+++ include/auth.h
@@ -4,10 +4,10 @@
 #include <string>
 
 namespace Auth {
-    // Check if a request contains valid Authorization token
+    // Check if  request contains valid Authorization token
     bool isAuthorized(const std::string& request);
 
-    // Limit repeated requests from the same IP 
+    // Limit repeated requests from same IP 
     bool isRateLimited(const std::string& ip);
 }
 
