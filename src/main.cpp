6eaf0340bceb62342c73d7a7dcffbce2269af1dd
Brian Le <brianle1018@gmail.com>
2025-06-03 16:05:23 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include "../include/proxy.h"
 #include <iostream>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
 
 int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+
     int port = 8080;                // Port that RebelGate will listen on
     std::string backendHost = "127.0.0.1"; // Backend server 
-    int backendPort = 9000;         // Backend server port
+    int backendPort = 9000;         // Port that backend server is running on
 
     std::cout << "Starting RebelGate on port " << port << "..." << std::endl;
 
     ProxyServer server(port, backendHost, backendPort);
     server.start(); // listens and starts requests
 
+    WSACleanup();
     return 0;
 }
